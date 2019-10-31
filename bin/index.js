7789b9847edbd6589139604d9bd1cbc772b7cb22
zangqikai <zangqikai@qutoutiao.net>
2019-10-31 15:46:00 +0800

    更新

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const program = require('commander');
 const { version } = require('../package.json');
 const { initProject, customizeProject, copyProject } = require('../lib/lifecycle');
