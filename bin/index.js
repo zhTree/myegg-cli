60d78775713c0677726a115b5c9d156e81a925be
zangqikai <zangqikai@qutoutiao.net>
2019-11-18 15:39:39 +0800

    添加创建文件工具

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 const program = require('commander');
 const { version } = require('../package.json');
-const { initProject, customizeProject, copyProject } = require('../lib/lifecycle');
+const {
+  initProject,
+  customizeProject,
+  copyProject,
+  checkFirst,
+  checkFolder,
+  checkFile,
+  addFile
+} = require('../lib/lifecycle');
 const { red, yellow } = require('chalk');
 const log = console.log;
+
+function check(type, name) {
+  if (checkFirst() || checkFolder(type) || checkFile(type, name)) return true;
+}
 
 program
   .version(version, '-v, --version')
@@ -17,16 +29,53 @@ program
     if (!sourceTarget) return;
     await customizeProject(projectName, sourceTarget);
     await copyProject(projectName, sourceTarget);
-    log(yellow(`
+    log(
+      yellow(`
       To run this project:
       cd ${projectName} && npm i && npm run dev
-    `));
+    `)
+    );
     // console.log(destination);
+  });
+
+program
+  .command('controller <controller-name>')
+  .description('添加控制器')
+  .action(async (name, destination) => {
+    if (check('controller', name)) return;
+    await addFile('controller', name);
+  });
+
+program
+  .command('service <service-name>')
+  .description('添加服务')
+  .action(async (name, destination) => {
+    if (check('service', name)) return;
+    await addFile('service', name);
+  });
+
+program
+  .command('model <model-name>')
+  .description('添加模型')
+  .action(async (name, destination) => {
+    if (check('model', name)) return;
+    await addFile('model', name);
+  });
+
+program
+  .command('bulk <bulk-name>')
+  .description('添加模型, 服务，控制器')
+  .action(async (name, destination) => {
+    if (check('model', name)) return;
+    if (check('service', name)) return;
+    if (check('controller', name)) return;
+    addFile('model', name);
+    addFile('service', name);
+    addFile('controller', name);
   });
 
 program.on('help', () => {
   console.log('');
 });
 
-program
-  .parse(process.argv);
+program.parse(process.argv);
--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,8 @@ const execa = require('execa');
 const { download } = require('./download');
 const { red, blue } = require('chalk');
 const log = console.log;
+const path = require('path');
+const fs = require('fs');
 
 async function exec(cmd) {
   try {
@@ -17,8 +19,29 @@ async function getGitInfo() {
   return await exec('git config user.name');
 }
 
+function checkInNpmProject() {
+  const cwd = process.cwd();
+  const packageJson = path.resolve(cwd, 'package.json');
+  return fs.existsSync(packageJson);
+}
+
+function checkHasFolder(folder) {
+  const cwd = process.cwd();
+  const folderPath = path.resolve(cwd, 'app', folder);
+  return fs.existsSync(folderPath) && fs.statSync(folderPath).isDirectory();
+}
+
+function checkHasFile(folder, file) {
+  const cwd = process.cwd();
+  const filePath = path.resolve(cwd, 'app', folder, `${file}.js`);
+  return fs.existsSync(filePath);
+}
+
 module.exports = {
   download,
   exec,
-  getGitInfo
+  getGitInfo,
+  checkInNpmProject,
+  checkHasFolder,
+  checkHasFile
 };
